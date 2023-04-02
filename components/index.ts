40706df58fd0bbe8469b56cb254fe899b8f72061
Bryan Flores <bryanflores12345@gmail.com>
2023-04-02 18:43:41 -0400

    Navbar component

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,7 @@
-import Input from '@/components/Input'
+import Input from './Input'
+import Navbar from './Navbar'
+import NavbarItem from './NavbarItem'
+import MobileMenu from './MobileMenu'
+import AccountMenu from './AccountMenu'
 
-export { Input }
+export { AccountMenu, Input, Navbar, NavbarItem, MobileMenu }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import { getSession, signOut } from 'next-auth/react'
+import { getSession } from 'next-auth/react'
 import { NextPageContext } from 'next'
-import useCurrentUser from '@/hooks/useCurrentUser'
+import { Navbar } from '@/components'
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context)
@@ -20,16 +20,10 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Home = () => {
-  const { data: user } = useCurrentUser()
-
   return (
-    <>
-      <h1 className='text-4xl text-green-500'>Netflix Clone</h1>
-      <p className='text-white'>Logged in as: {user?.email}</p>
-      <button className='h-10 w-full bg-white' onClick={() => signOut()}>
-        Logout
-      </button>
-    </>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
